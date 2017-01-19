e0d1431d480e0717ef5a30621103842d37e4773e
rafald121 <rafald121@gmail.com>
2017-01-19 01:17:14 +0100

    add: notdone method that return quantity of not done tasks NOT SURE IF WORK
    before test

--- todoAPI.py
+++ todoAPI.py
@@ -3,12 +3,36 @@ from flask import Flask, request, json, Response
 
 app = Flask(__name__)
 
+# OPTIONAL TO ADD LATER
+# getTimeAndData to single task when client send request
+
 usersDict = {}
-usersDict['rafal'] = {'token': 'rafaltoken', 'password': 'rafal', 'userID': 1}
-usersDict['pawel'] = {'token': 'paweltoken', 'password': 'pawel', 'userID': 2}
-usersDict['piotr'] = {'token': 'piotrtoken', 'password': 'piotr', 'userID': 3}
+usersDict['rafal'] = {'token': 'rafaltoken', 'password': 'rafal', 'userID': 1, 'tasks': {}}
+# usersDict['pawel'] = {'token': 'paweltoken', 'password': 'pawel', 'userID': 2}
+# usersDict['piotr'] = {'token': 'piotrtoken', 'password': 'piotr', 'userID': 3}
 
 tasksDict = {}
+tasksDict[1] = {"title": "Projekt z programowania internetowego",
+                "details": "Projekt, w którym piszemy własną usługę sieciową + 2 rodzaje klientów",
+                "timeToDo": "30.01.2016",
+                "tag": ["school"],
+                "done": 0,
+                "id": 1
+                }
+tasksDict[2] = {"title": "Sprzątnięćie kuchni",
+                "details": "Dokładne wyczyszczenie kuchenki i mikrofali",
+                "timeToDo": "15.02.2016",
+                "tag": ["home"],
+                "done": 0,
+                "id": 2
+                }
+tasksDict[3] = {"title": "Specyfikacja dla klienta",
+                "details": "Napisanie szczegółowej specyfikacji dla klienta dotyczącej aplikacji",
+                "timeToDo": "25.09.2016",
+                "tag": ["work"],
+                "done": 0,
+                "id": 3
+                }
 
 
 @app.route('/login', methods=['POST'])
@@ -19,10 +43,10 @@ def login():
     requestDataRead = False
 
     try:
-        requestData = json.loads(request.data)
+        requestData = json.load(request.data)
         requestDataRead = True
     except:
-        responseData = {'error': 'bad request'}
+        responseData = {'error': 'nie moglem odczytac danych z requestu, ktory wyslal klient'}
 
     if requestDataRead:
         if 'login' in requestData and 'password' in requestData:
@@ -32,10 +56,10 @@ def login():
                         'info': "OK",
                         'token': usersDict[requestData['login']]['token'],
                         'userID': usersDict[requestData['login']]['userID']
-                        }
+                    }
                     status = 200
                 else:
-                    responseData = {'error': 'Nie poprawny login lub haslo'}
+                    responseData = {'error': 'Niepoprawny login lub haslo'}
             else:
                 responseData = {'error': 'Brak uzytkownika o podanym loginie w bazie danych'}
         else:
@@ -45,10 +69,50 @@ def login():
     responseHeaders = {'Content-Type': 'application/json'}
     response = Response(responseJsonData,
                         status=status,
-                        mimetype = "application/json",
+                        mimetype="application/json",
                         headers=responseHeaders)
     return response
 
+
+@app.route("/notdone", methods=['GET'])
+def notdone():
+    requestData = None
+    requestDataRead = False
+    responseJsonData = None
+    status = 400
+    responseData = None
+
+    try:
+        requestData = json.load(request.data)
+        requestDataRead = True
+    except:
+        responseData = {"error": "bad token give to request"}
+
+    if requestDataRead:
+        # TODO add searching login based on given in request token
+
+        if requestData['token'] == usersDict['rafal']['token']:
+
+            # TODO dodac funkcje, ktora liczy niewykonane zadania
+            responseData = {"undone": '3'}
+            status = 200
+        else:
+            responseData = {
+                "error": "couldn't match login to requested token(probably there isn't any user with requested token)"}
+    else:
+        responseData = {"error": "bad syntax of request(bad oken has been given"}
+
+    responseJsonData = json.dumps(responseData)
+    responseHeaders = {'Content-Type': 'application/json'}
+    response = Response(responseJsonData,
+                        status=status,
+                        mimetype="application/json",
+                        headers=responseHeaders)
+
+    return response
+
+
+# def countUndoneTask():
 
 
 if __name__ == '__main__':
